b104498df7c33dbca03917f222a501f7c2bd7311
HenriqueGoncalves22 <henrique.gonc.gamer@gmail.com>
2024-10-03 19:27:14 -0300

    first commit

---
+++ src/test/java/br/com/etechas/pw_relacionamento/PwRelacionamentoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.etechas.pw_relacionamento;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwRelacionamentoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
